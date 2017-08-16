ff06e7a31ac784f942135e842b87fc79b04ddc5c
ZhangYu <2783789733@qq.com>
2017-08-16 21:58:21 +0800

    first commit.

---
+++ app/src/main/java/com/twoexample/coolweather/db/City.java
@@ -0,0 +1,48 @@
+package com.twoexample.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by 27837 on 2017/8/16.
+ */
+
+public class City extends DataSupport {
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private String provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public String getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(String provinceId) {
+        this.provinceId = provinceId;
+    }
+
+
+}
